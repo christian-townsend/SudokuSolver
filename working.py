49a5dd9c70422bba684a9cd1f064cd25082eb32d
christian-townsend <51427447+christian-townsend@users.noreply.github.com>
2020-02-03 11:23:30 +1100

    Add files via upload

--- working.py
+++ working.py
@@ -10,8 +10,26 @@ board = [
     [0,4,9,2,0,6,0,0,7]
 ]
 
-def valid(bo, num, pos):
+def solve(bo):
 
+    find = find_empty(bo)
+    if not find:
+        return True
+    else:
+        row, col = find
+
+    for i in range (1,10):
+        if valid(bo, i, (row, col)):
+            bo[row][col] = i
+
+            if solve(bo):
+                return True
+
+            bo[row][col] = 0
+
+    return False
+
+def valid(bo, num, pos):
     # Check row
     for i in range(len(bo[0])):
         if bo[pos[0]][i] == num and pos[1] != i:
@@ -27,6 +45,10 @@ def valid(bo, num, pos):
 
     for i in range (box_y * 3, box_y * 3 + 3):
         for j in range(box_x * 3, box_x * 3 + 3):
+            if bo[i][j] == num and (i,j) != pos:
+                return False
+
+    return True
 
 def print_board(bo):
     for i in range (len(bo)):
@@ -42,10 +64,16 @@ def print_board(bo):
             else:
                 print(str(bo[i][j]) + " ", end="")
 
-print_board(board)
-
 def find_empty(bo):
     for i in range(len(bo)):
         for j in range(len(bo[0])):
             if bo[i][j] == 0:
                 return (i, j)  # row, col
+    return None
+
+print_board(board)
+print("- - - - - - - - - - - - - ")
+print("SOLVED BOARD")
+print("- - - - - - - - - - - - - ")
+solve(board)
+print_board(board)
